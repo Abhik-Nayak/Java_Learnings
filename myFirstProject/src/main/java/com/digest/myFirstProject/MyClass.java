f9877468fd473980809a2a5ec6a727cf8f5e613a
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-06-03 12:29:07 +0530

    code push

--- myFirstProject/src/main/java/com/digest/myFirstProject/MyClass.java
+++ myFirstProject/src/main/java/com/digest/myFirstProject/MyClass.java
@@ -1,15 +1,25 @@
 package com.digest.myFirstProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 public class MyClass {
 
-    @GetMapping("/abc") // annotation
-    public String SayHello() { // method
-        return "Hello, World!";
+    @Autowired
+    private GreetingService greetingService;
+
+    @Autowired
+    private TimeService timeService;
+
+    @GetMapping("/abc")
+    public String SayHello() {
+        return greetingService.getGreeting();
+    }
+
+    @GetMapping("/time")
+    public String showTime() {
+        return timeService.getCurrentTime();
     }
 }
