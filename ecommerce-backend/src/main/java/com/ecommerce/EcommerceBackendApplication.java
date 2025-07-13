2636cde30ed7d3974fc2a076b23fb596fc156296
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-07-13 06:14:49 +0530

    changes pushed

--- ecommerce-backend/src/main/java/com/ecommerce/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/ecommerce/EcommerceBackendApplication.java
@@ -2,16 +2,6 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-class HelloController {
-	@GetMapping("/")
-	public String hello() {
-		return "Hello, World!";
-	}
-}
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
