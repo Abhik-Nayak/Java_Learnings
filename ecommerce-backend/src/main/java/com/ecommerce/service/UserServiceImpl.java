78e11a7619bda1336f68648c0eba431dfc5fd2f8
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-07-13 06:32:35 +0530

    code push api signup setup
    User can create User /signup without DTO setup

---
+++ ecommerce-backend/src/main/java/com/ecommerce/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ecommerce.service;
+
+import com.ecommerce.model.User;
+import com.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User registerUser(User user) {
+        // In future: check if user exists, encode password
+        return userRepository.save(user);
+    }
+}
