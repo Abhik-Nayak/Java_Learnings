fb09bb4a59a1ebce3c28e3893653ebfd0260063a
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-07-19 07:07:36 +0530

    code push

--- ecommerce-backend/ecommerce-backend/src/main/java/com/abhi/ecommerce/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abhi.ecommerce.repository;
-
-import com.abhi.ecommerce.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
