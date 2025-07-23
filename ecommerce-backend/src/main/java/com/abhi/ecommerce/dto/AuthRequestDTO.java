df5a2100d428a99cf91d93a575394fc6669c1492
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-07-23 15:51:03 +0530

    Request and response DTO

---
+++ ecommerce-backend/src/main/java/com/abhi/ecommerce/dto/AuthRequestDTO.java
@@ -0,0 +1,17 @@
+package com.abhi.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthRequestDTO {
+    private String email;
+    private String password;
+}
